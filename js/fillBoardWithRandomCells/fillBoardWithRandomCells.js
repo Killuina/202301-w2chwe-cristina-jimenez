dca57e477ebecf95bceb10f26ee19f89ba4523ac
Cristina <cristinajmartin98@gmail.com>
2023-01-22 19:13:31 +0100

    Make fillBoardWithRandomCells function to return the filled board

--- js/fillBoardWithRandomCells/fillBoardWithRandomCells.js
+++ js/fillBoardWithRandomCells/fillBoardWithRandomCells.js
@@ -1,16 +1,16 @@
 import Cell from "../Cell/Cell.js";
 import generateBoard from "../generateBoard/generateBoard.js";
 
-const board = generateBoard(3, 3, null);
-
 const fillBoardWithRandomCells = () => {
+  const board = generateBoard(3, 3, null);
   board.forEach((columns, position) => {
     board[position].forEach((rows, rowIndex) => {
       board[position][rowIndex] = new Cell();
     });
   });
+  return board;
 };
 
 fillBoardWithRandomCells();
 
-export { fillBoardWithRandomCells, board };
+export { fillBoardWithRandomCells };
